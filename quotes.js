33f186cf1bd7a67cfc84e762c68340ef2b75b706
Makayla Moster <monkmonster96@gmail.com>
2021-04-03 12:01:53 -0400

    test

--- quotes.js
+++ quotes.js
@@ -1,13 +1,13 @@
 
 var quotes = [
-  'To the stars who listen - and the dreams that are answered <br><br> Sarah J. Maas',
-  'It does not do to dwell on dreams and forget to live \n\nJ.K. Rowling',
-  'When in doubt, go to the library \n\nRon Weasley',
-  'But you know, happiness can be found even in the darkest of times, if one only remembers to turn on the light \n\nAlbus Dumbledore',
-  'And now, let us step out into the night and pursue that flighty temptress, adventure \n\nAlbus Dumbledore',
-  'The only thing that you absolutely have to know, is the location of the library \n\nAlbert Einstein',
-  'Courage, dear heart \n\nC.S. Lewis',
-  'When people say impossible, they usually mean improbable \n\nLeigh Bardugo'
+  'To the stars who listen - and the dreams that are answered <br><br>Sarah J. Maas',
+  'It does not do to dwell on dreams and forget to live <br><br>J.K. Rowling',
+  'When in doubt, go to the library <br><br>Ron Weasley',
+  'But you know, happiness can be found even in the darkest of times, if one only remembers to turn on the light <br><br>Albus Dumbledore',
+  'And now, let us step out into the night and pursue that flighty temptress, adventure <br><br>Albus Dumbledore',
+  'The only thing that you absolutely have to know, is the location of the library <br><br>Albert Einstein',
+  'Courage, dear heart <br><br>C.S. Lewis',
+  'When people say impossible, they usually mean improbable <br><br>Leigh Bardugo'
 ]
 
 function newQuote() {
