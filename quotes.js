fe9318499eb05114ad6723b3c139def4f7ba47e0
Makayla Moster <monkmonster96@gmail.com>
2021-04-03 11:10:59 -0400

    quotes test

---
+++ quotes.js
@@ -0,0 +1,10 @@
+
+var quotes = [
+  'Hogwarts will always be there to welcome you home - J.K. Rowling',
+  'To the stars who listen - and the dreams that are answered - Sarah J. Maas'
+]
+
+function newQuote() {
+  var randomNum = Math.floor(Math.random() (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumer];
+}
