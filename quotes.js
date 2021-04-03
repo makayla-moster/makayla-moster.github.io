79ae3b5cea739a91914b407969f5890399821090
Makayla Moster <monkmonster96@gmail.com>
2021-04-03 18:46:51 -0400

    more quotes

--- quotes.js
+++ quotes.js
@@ -12,7 +12,12 @@ var quotes = [
   'Worrying means you suffer twice. <br><small>~ Newt Scamander</small>',
   'It is better to walk alone, then with a crowd going in the wrong direction.<br><small>~ Herman Siu</small>',
   'Keep your eyes on the stars, and your feet on the ground. <br><small>~ Teddy Roosevelt</small>',
-  'To live will be an awfully big adventure. <br><small>~ J.M. Barrie</small>'
+  'To live will be an awfully big adventure. <br><small>~ J.M. Barrie</small>',
+  'The most wasted of all days is one without laughter. <br><small>~ E.E. Cummings</small>',
+  'Remember kids, the only difference between screwing around and science, is writing it down. <br><small>~ Adam Savage</small>',
+  'You could rattle the stars. You could do anything, if only you dared. <br><small>~ Sarah J. Maas</small>',
+  'Weeds are flowers too, once you get to know them. <br><small>~ A.A. Milne</small>',
+  'One of the advantages of being disorganized is that one is always having surprising discoveries. <br><small>~ A.A. Milne</small>'
   // "We are all broken, that's how the light gets in. <br><small>~ Ernest Hemingway</small>",
   // "If you aren't in over your head, how do you know how tall you are? <br><small>~ T.S. Eliot</small>"
 ]
