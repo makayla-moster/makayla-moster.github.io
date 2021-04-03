515f5b6e427567f446be75c1b888f7200fdfc3f4
Makayla Moster <monkmonster96@gmail.com>
2021-04-03 12:00:55 -0400

    test

--- quotes.js
+++ quotes.js
@@ -1,6 +1,6 @@
 
 var quotes = [
-  'To the stars who listen - and the dreams that are answered \n\nSarah J. Maas',
+  'To the stars who listen - and the dreams that are answered <br><br> Sarah J. Maas',
   'It does not do to dwell on dreams and forget to live \n\nJ.K. Rowling',
   'When in doubt, go to the library \n\nRon Weasley',
   'But you know, happiness can be found even in the darkest of times, if one only remembers to turn on the light \n\nAlbus Dumbledore',
