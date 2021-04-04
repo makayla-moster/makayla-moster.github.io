64b7750b3c89fde5c3b59f642abe257f94f6c858
Makayla Moster <monkmonster96@gmail.com>
2021-04-03 22:27:48 -0400

    MORE

--- quotes.js
+++ quotes.js
@@ -9,7 +9,7 @@ var quotes = [
   'Courage, dear heart. <br><small>~ C.S. Lewis</small>',
   'When people say impossible, they usually mean improbable. <br><small>~ Leigh Bardugo</small>',
   'The best way out is always through. <br><small>~ Robert Frost</small>',
-  'Worrying means you suffer twice. <br><small>~ Newt Scamander</small>',
+  'My philosophy is that worrying means you suffer twice. <br><small>~ Newt Scamander</small>',
   'It is better to walk alone, then with a crowd going in the wrong direction.<br><small>~ Herman Siu</small>',
   'Keep your eyes on the stars, and your feet on the ground. <br><small>~ Teddy Roosevelt</small>',
   'To live will be an awfully big adventure. <br><small>~ J.M. Barrie</small>',
@@ -17,7 +17,19 @@ var quotes = [
   'Remember kids, the only difference between screwing around and science, is writing it down. <br><small>~ Adam Savage</small>',
   'You could rattle the stars. You could do anything, if only you dared. <br><small>~ Sarah J. Maas</small>',
   'Weeds are flowers too, once you get to know them. <br><small>~ A.A. Milne</small>',
-  'One of the advantages of being disorganized is that one is always having surprising discoveries. <br><small>~ A.A. Milne</small>'
+  'One of the advantages of being disorganized is that one is always having surprising discoveries. <br><small>~ A.A. Milne</small>',
+  'The scariest moment is always just before you start. After that, things can only get better. <br><small>~ Stephen King</small>',
+  'Fairy tales are more than true: not because they tell us dragons exist, but because they tell us that dragons can be beaten. <br><small>~ Neil Gaiman</small>',
+  'I am gone quite mad with the knowledge of accepting the overwhelming number of things I can never know, places I can never go, and people I can never be. <br><small>~ Sylvia Plath</small>',
+  'Some day you will be old enough to start reading fairy tales again. <br><small>~ C.S. Lewis</small>',
+  'Just close your eyes and pretend it&#39;s all a dream. That&#39;s how I get by. <br><small>~ Captain Jack Sparrow</small>',
+  'The problem is not the problem; the problem is your attitude about the problem. <br><small>~ Captain Jack Sparrow</small>',
+  'I wash my hands of this weirdness. <br><small>~ Captain Jack Sparrow</small>',
+  'I never look back, darling. It distracts from the now. <br><small>~ Edna Mode</small>',
+  'Part of the journey is the end. <br><small>~ Tony Stark</small>',
+  'The past can hurt. But the way I see it, you can either run from it, or learn from it. <br><small>~ Rafiki</small>',
+  'Inconceivable! <br><small>~ Vizzini</small>',
+  'You&#39;re only given a little spark of madness. You mustn&#39;t lose it. <br><small>~ Robin Williams</small>'
   // "We are all broken, that's how the light gets in. <br><small>~ Ernest Hemingway</small>",
   // "If you aren't in over your head, how do you know how tall you are? <br><small>~ T.S. Eliot</small>"
 ]
