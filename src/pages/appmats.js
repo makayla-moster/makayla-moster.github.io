5465e7b27c99f57e99de29c67b6c723070dbd596
makayla-moster <makaylamoster@gmail.com>
2024-08-28 14:59:16 -0400

    u

--- src/pages/appmats.js
+++ src/pages/appmats.js
@@ -6,8 +6,10 @@ import { Link } from 'gatsby'
 const JobPage = () => {
     return (
         <Layout pageTitle="Application Materials">
-            <p className='pb-5'>I anticipate graduating in May 2025 and I will be looking for academic positions for Fall 2025.</p>
-            <p className='pb-5'>I am currently working on my application materials, they will be posted here soon!</p>
+            <p >I anticipate graduating in May 2025 and I will be looking for academic positions for Fall 2025.</p><p className='pb-5'>My application materials can be found below.</p>
+            <span className='text-cyan-500 hover:opacsity-60 font-archivoNarrow text-2xl pb-5'><Link to="https://github.com/makayla-moster/pdfs/blob/master/cv/Moster_Makayla_CV.pdf?raw=true" target="_blank">CV</Link></span>
+            <span className='text-cyan-500 hover:opacsity-60 font-archivoNarrow text-2xl pb-5'><Link to="https://github.com/makayla-moster/pdfs/blob/master/application/Moster_TeachingStatement.pdf?raw=true" target="_blank">Teaching Statement</Link></span>
+            <span className='text-cyan-500 hover:opacsity-60 font-archivoNarrow text-2xl pb-5'><Link to="https://github.com/makayla-moster/pdfs/blob/master/application/Moster_ResearchStatement.pdf?raw=true" target="_blank">Research Statement</Link></span>
             <p className='pb-5'>If you are interested in collaborating or know of a position I would be right for, please contact me at <span className="text-cyan-500 hover:opacity-60"><Link to="mailto:makaylamoster@gmail.com">makaylamoster@gmail.com</Link></span>!</p>
         </Layout>
     )
